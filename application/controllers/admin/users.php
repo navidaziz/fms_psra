f642926fb57df31f48d6d66cb679db16f156538e
navidaziz98 <navidaziz98@gmail.com>
2022-06-14 16:34:38 +0500

    remove extra file

--- application/controllers/admin/users.php
+++ application/controllers/admin/users.php
@@ -294,7 +294,7 @@ class Users extends Admin_Controller
             if ($user_id) {
 
                 $this->session->set_flashdata("msg_success", $this->lang->line("update_msg_success"));
-               // echo "success";
+                // echo "success";
                 exit();
             } else {
                 $_POST['id'] = $user_id;
@@ -334,118 +334,6 @@ class Users extends Admin_Controller
         }
     }
 
-
-    /**
-     * logout a user
-     */
-    public function logout()
-    {
-        //remove the card .....
-        $query = "DELETE FROM `item_cart` WHERE `user_id`='" . $this->session->userdata("user_id") . "' and `user_unique_id`='" . $this->session->userdata("user_unique_id") . "'";
-        $this->db->query($query);
-
-        $this->user_m->logout();
-        redirect(ADMIN_DIR . "users/login");
-    }
-    //-----------------------------------------------------
-
-    /**
-     * function to login a user
-     */
-    public function login()
-    {
-
-        //$this->data['captcha'] = $this->captcha(); 
-        //check if the user is already logedin
-        if ($this->user_m->loggedIn() == TRUE) {
-            redirect(ADMIN_DIR . "users/view");
-        }
-
-        //load other models
-        $this->load->model("role_m");
-        $this->load->model("module_m");
-
-        $validations = array(
-            /*array(
-                'field' =>  'user_email',
-                'label' =>  'Email Address',
-                'rules' =>  'valid_email|required'
-            ),
-            */
-            array(
-                'field' =>  'user_password',
-                'label' =>  'Password',
-                'rules' =>  'required'
-            )
-        );
-        $this->form_validation->set_rules($validations);
-        if ($this->form_validation->run() === TRUE) {
-
-            $input_values = array(
-                'user_name' => $this->input->post("user_email"),
-                'user_password' => $this->input->post("user_password")
-
-            );
-
-            //get the user
-            $user = $this->user_m->getBy($input_values, TRUE);
-            //var_dump($user);
-            //exit;
-
-            if (count($user) > 0 and $user->role_id != 2 and $user->role_id != 24 and $user->role_id != 23) {
-
-                //
-                $role_homepage_id = $this->role_m->getCol("role_homepage", $user->role_id);
-                $role_homepage_parent_id = $this->module_m->getCol("parent_id", $role_homepage_id);
-
-                //now create homepage path
-                $homepage_path = "";
-                if ($role_homepage_parent_id != 0) {
-                    $homepage_path .= $this->module_m->getCol("module_uri", $role_homepage_parent_id) . "/";
-                }
-                $homepage_path .= $this->module_m->getCol("module_uri", $role_homepage_id);
-
-                $fields = "roles.*";
-                $join  = array();
-                $where = "roles.role_id = $user->role_id";
-                $role = $roles = $this->role_m->joinGet($fields, "roles", $join, $where);
-
-                //get user projects  by role id
-
-
-
-                $user_data = array(
-                    "user_id"  => $user->user_id,
-                    "user_email" => $user->user_email,
-                    "user_title" => $user->user_title,
-                    "role_id" => $user->role_id,
-                    "role_level" =>  $role[0]->role_level,
-                    "district_id" => '',
-                    "role_homepage_id" => $role_homepage_id,
-                    "role_homepage_uri" => $homepage_path,
-                    "ngo_id" => '',
-                    "user_image" => $user->user_image,
-                    "user_unique_id" => md5(uniqid(rand(), TRUE)),
-                    "logged_in" => TRUE
-                );
-
-
-                //add to session
-                $this->session->set_userdata($user_data);
-                //var_dump($this->session->userdata);
-                //exit;
-                $this->session->set_flashdata('msg_success', "<strong>" . $user->user_title . '</strong><br/><i>welcome to admin panel</i>');
-                redirect(ADMIN_DIR . $homepage_path);
-            } else {
-                $this->session->set_flashdata('msg', 'User Name or Password is incorrect or Your Are not Allowed to Access this Admin Section ');
-                redirect(ADMIN_DIR . "users/login");
-            }
-        } else {
-
-            $this->data['title'] = "Login to dashboard";
-            $this->load->view(ADMIN_DIR . "users/login", $this->data);
-        }
-    }
 
 
     public function update_profile()
